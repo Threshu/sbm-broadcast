5eb2e5af3c621a259ed4b3122dc3f3ed2b05806c
Threshu <szymon.banach.dev@gmail.com>
2023-10-28 16:51:39 +0200

    format main

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -4,17 +4,17 @@
  * Automatically included in `./src/main.ts`
  */
 
-import vuetify from './vuetify'
-import pinia from '../store'
-import router from '../router'
-import i18n from './i18n'
+import vuetify from './vuetify';
+import pinia from '../store';
+import router from '../router';
+import i18n from './i18n';
 
-import type { App } from 'vue'
+import type { App } from 'vue';
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
-    .use(i18n)
+    .use(i18n);
 }
