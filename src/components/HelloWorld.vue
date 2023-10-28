9b0e9a6ca2ab86b9df00d3a762f1807b1c7af7a8
Threshu <szymon.banach.dev@gmail.com>
2023-10-28 21:22:51 +0200

    reading pgn and fen convert

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,9 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Illegal move</h1>
-
-      <div class="py-14" />
-      <v-stepper :items="['Step 1', 'Step 2', 'Step 3']">
-  <template v-slot:item.1>
-    <v-card title="Step One" flat>1</v-card>
-  </template>
-
-  <template v-slot:item.2>
-    <v-card title="Step Two" flat>2</v-card>
-  </template>
-
-  <template v-slot:item.3>
-    <v-card title="Step Three" flat>    <section>
     <div>
+      <v-btn color="primary" @click="downloadAndLogPgn">Pobierz PGN</v-btn>
+
       <button @click="boardAPI?.toggleOrientation()">
         Toggle orientation
       </button>
@@ -27,33 +11,68 @@
       <button @click="boardAPI?.undoLastMove()">Undo</button>
       <button @click="boardAPI?.toggleMoves()">Threats</button>
     </div>
+    {{ fen }}
     <TheChessboard
       :board-config="boardConfig"
+      :fen="fen"
       @board-created="(api) => (boardAPI = api)"
-      @checkmate="handleCheckmate"
     />
-  </section></v-card>
-  </template>
-</v-stepper>
-    </v-responsive>
   </v-container>
 </template>
-
 <script setup lang="ts">
 import { TheChessboard } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 import type { BoardApi, BoardConfig } from 'vue3-chessboard';
+import { ref } from 'vue';
+import { Chess } from 'chess.js';
+
 
 let boardAPI: BoardApi;
 const boardConfig: BoardConfig = {
   coordinates: false,
 };
 
-function handleCheckmate(isMated: string) {
-  if (isMated === 'w') {
-    alert('Black wins!');
-  } else {
-    alert('White wins!');
+const fen = ref(''); // Tutaj przechowywany będzie FEN
+
+
+// Logika odpowiedzialna za pobieranie i wyświetlanie zawartości pliku .pgn
+async function downloadAndLogPgn() {
+  try {
+    const currentLocation = window.location.href;
+    const pgnFilePath = currentLocation.includes('localhost')
+      ? `${location.protocol}//${location.hostname}:${location.port}${location.pathname.replace(/\/[^/]*$/, '/games.pgn')}`
+      : '/games.pgn';
+
+    // Pobieranie pliku .pgn
+    const response = await fetch(pgnFilePath);
+    if (!response.ok) {
+      throw new Error("Błąd sieci przy próbie pobrania pliku.");
+    }
+
+    // Odczytywanie i logowanie zawartości pliku .pgn
+    const pgnContent = await response.text();
+    console.log(pgnContent);
+
+
+
+
+      function convertPgnToFen(pgn: any) {
+      const chess = new Chess();
+      const success = chess.loadPgn(pgn);
+console.log(success);
+
+
+      fen.value = chess.fen();
+      console.log(fen.value);
+    }
+
+
+
+      convertPgnToFen(pgnContent);
+
+
+  } catch (error) {
+    console.error("Wystąpił błąd podczas pobierania pliku .pgn:", error);
   }
 }
 </script>
